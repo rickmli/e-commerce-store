113dc9ac6689ed66bf545a90c2a030c101067705
Rick Li <79388294+rickmli@users.noreply.github.com>
2026-01-05 22:17:23 -0800

    implemented sign-in method which intakes email and password from user and return refreshed access and refresh token to user, the token will be used in middleware to prefetch user's identity before accessing methods that are limited by user's authority levels

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -23,13 +23,10 @@ export const signup = async (req, res) => {
 
     setCookies(res, accessToken, refreshToken);
     res.status(201).json({
-      user: {
-        _id: newUser._id,
-        name: newUser.name,
-        email: newUser.email,
-        role: newUser.role,
-      },
-      message: "User created successfully",
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      role: newUser.role,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -48,6 +45,36 @@ export const signout = async (req, res) => {
 
     res.status(200).json({
       message: "Logged out successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const passwordMatched = await user.comparePassword(password);
+    if (!passwordMatched) {
+      return res.status(400).json({ message: "Password incorrect" });
+    }
+
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup, signout } from "../controllers/authController.js";
+import { signup, signout, signin } from "../controllers/authController.js";
 
 const router = express();
 
 router.post("/signup", signup);
 router.post("/signout", signout);
+router.post("/signin", signin);
 
 export default router;
