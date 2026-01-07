95e8b6ccfe60bfb575440d74bb6518b780ab42c2
Rick Li <79388294+rickmli@users.noreply.github.com>
2026-01-06 22:05:31 -0800

    exstablished another approach for async handler for practice purpose

--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -1,5 +1,15 @@
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
+// the promise approach
+// const asyncHandler = (fn) => (req, res, next) => {
+//   Promise.resolve(fn(req, res, next)).catch(next);
+// };
+
+// the try-catch approach
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next); // 等待 fn 执行，如果 fn 是异步的
+  } catch (err) {
+    next(err); // 捕获错误并传递
+  }
 };
 
 export default asyncHandler;
