619b26a3c8ac8daffdf170de1598165ea8043b72
Rick Li <79388294+rickmli@users.noreply.github.com>
2026-01-19 00:02:47 -0800

    integrated delete cart item from cart

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -71,7 +71,7 @@ export const useCartStore = create((set, get) => ({
     }
   },
   removeFromCart: async (productId) => {
-    await axios.delete(`/cart`, { data: { productId } });
+    await axios.delete(`/cart/${productId}`);
     set((prevState) => ({
       cart: prevState.cart.filter((item) => item._id !== productId),
     }));
